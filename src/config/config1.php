d9d43d647999c88b688a3df4bcfb539f88db6a1d
Yampiers Quispe <169065995+Jhampiers@users.noreply.github.com>
2025-05-08 17:30:59 -0500

    init

--- src/config/config1.php
+++ src/config/config1.php
@@ -1,14 +1,14 @@
 <?php
 
 const BD_HOST = 'localhost';
-const BD_NAME = '';
-const BD_USER = '';
-const BD_PASSWORD = '';
+const BD_NAME = 'inventario2';
+const BD_USER = 'root';
+const BD_PASSWORD = 'root';
 
 const BD_CHARSET = 'utf8';
 
 //ingresar link de proyecto
-const BASE_URL = '';
-const BASE_URL_SERVER = '';
+const BASE_URL = 'http://localhost/inventario2/';
+const BASE_URL_SERVER = 'http://localhost/inventario2/';
 
 ?>
